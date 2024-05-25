3e43450fa8dd570faa7aa1b3ace4b228c830f24c
Anass <seghiranas3@gmail.com>
2024-05-25 02:07:31 +0100

    added files to the repository

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter, AiFillFacebook} from 'react-icons/ai';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>2024 Sound. All rights reserverd</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+        <AiFillFacebook />
+      </p>
+    </div>
+  )
+}
+
+export default Footer
